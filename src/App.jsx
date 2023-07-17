450f1eebb09268201bb48fadebd01d3a6ac40d94
neilramch <neilramch@gmail.com>
2023-07-17 19:12:16 -0400

    added projects page template with animation effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Home from './components/Home'
 import './App.css'
 import Projects from './components/Projects'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also load CSS styles from CDN
+
 
 function App() {
+
+  AOS.init();
+
   return (
     <>
     <Home />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,57 @@ function Projects() {
     <>
         <div className='projects-container'>
 
+            <h3>My Projects</h3>
+
+
+            <div className='my-projects'>
+
+
+            {/* First project */}
+
+            <div className='project'>
+
+                <div className='project-image' data-aos='fade-right'></div>
+                <div className="project-content" data-aos='fade-left'>
+                    <h2>Title</h2>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            {/* Second project */}
+
+              <div className='project'>
+
+                <div className='project-image' data-aos='fade-right'></div>
+                <div className="project-content" data-aos='fade-left'>
+                    <h2>Title</h2>
+                    <p>Description</p>
+                </div>
+            </div>
+
+
+
+                
+
+            {/* Third project */}
+            <div className='project'>
+
+                <div className='project-image' data-aos='fade-right'></div>
+
+                <div className="project-content" data-aos='fade-left'>
+                    <h2>Title</h2>
+                    <p>Description</p>
+                </div>
+
+            </div>
+
+            
+
+            
+            </div>
+
         </div>
+
 
     </>
   )
